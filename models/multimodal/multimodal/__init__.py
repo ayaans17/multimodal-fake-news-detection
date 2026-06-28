3dff1c15d7a559ef86c560d8bce3ad5f297d11cc
Ayaan <71969269+ayaans17@users.noreply.github.com>
2026-06-28 15:08:14 +0100

    Delete models/multimodal/multimodal directory

--- models/multimodal/multimodal/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Multimodal models."""
-
-from src.models.multimodal.distilbert_clip import DistilBertClipModel
-from src.models.multimodal.distilbert_clip_fusion import AttentionFusion, MultimodalFakeNewsModel
-
-__all__ = ["AttentionFusion", "DistilBertClipModel", "MultimodalFakeNewsModel"]
