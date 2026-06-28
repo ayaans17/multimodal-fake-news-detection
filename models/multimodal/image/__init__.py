90f025dcafedcc862a0252255cf68cc5a320eaae
Ayaan <71969269+ayaans17@users.noreply.github.com>
2026-06-28 15:08:02 +0100

    Delete models/multimodal/image directory

--- models/multimodal/image/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Image model helpers."""
-
-from src.models.image.clip import ClipEmbeddingDataset, collate_clip_batch, load_image
-
-__all__ = ["ClipEmbeddingDataset", "collate_clip_batch", "load_image"]
